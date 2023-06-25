1796884252d654d0b99496dee4237e86f983636a
Leesuyeoun <sssiuir@gmail.com>
2023-06-25 22:06:22 +0900

    Cex3

--- Cex3/main.c
+++ Cex3/main.c
@@ -1,14 +1,18 @@
 //
 //  main.c
 //  Cex3
-//
+//  지역변수 소스코드
 //  Created by 이수연 on 2023/06/25.
 //
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
+void main() {
     // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    int a = 3, b = 4;
+    {
+        int a = 5;
+        printf("%d, %d \n", a, b);
+    }
+    printf("%d, %d \n", a, b);
 }
